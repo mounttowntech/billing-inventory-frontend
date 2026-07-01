48ded13a7d5a871942f57e6a937d53ceccb31745
mounttowntech-vinoth <mounttowntech@gmail.com>
2026-07-01 14:55:21 +0530

    update login api

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import API from "../../services/api";
 
 export const loginApi = async (data) => {
-  const response = await API.post("/auth/login", data);
+  const response = await API.post("/users/login", data);
   return response.data;
 };
