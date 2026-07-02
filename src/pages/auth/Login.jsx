39fa41e9d54e93c6a2309282ccb02a8ca9072c07
mounttowntech <mounttowntech@gmail.com>
2026-07-02 11:48:01 +0530

    Revert "Frontendpages"

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,11 +1,9 @@
 import { useDispatch, useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { loginValidation } from "../../validations/loginValidation";
 import { loginUser } from "../../features/auth/authSlice";
-import Input from "../../components/common/Input";
-import "./Login.css"
 
 const Login = () => {
   const dispatch = useDispatch();
@@ -35,29 +33,15 @@ const Login = () => {
 
         {error && <p className="error">{error}</p>}
 
-        <Input
-          label="Email"
-          name="email"
-          type="email"
-          placeholder="Email"
-          register={register}
-          error={errors.email?.message}
-        />
-        <Input
-          label="Password"
-          name="password"
-          type="password"
-          placeholder="Password"
-          register={register}
-          error={errors.password?.message}
-        />
+        <input placeholder="Email" {...register("email")} />
+        <p className="error">{errors.email?.message}</p>
+
+        <input type="password" placeholder="Password" {...register("password")} />
+        <p className="error">{errors.password?.message}</p>
+
         <button disabled={loading}>
           {loading ? "Logging in..." : "Login"}
         </button>
-        {/* new to register */}
-        <Link to="/register" className="register-link">
-          <p>New to Register? Click here</p>
-        </Link>
       </form>
     </div>
   );
