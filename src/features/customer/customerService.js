4b5a776c6595e0fecaf343564e606934b5cd58f2
vikash-mounttowntech <mounttowntech@gmail.com>
2026-07-07 12:09:53 +0530

    CustomerList.jsx fully completed

--- src/features/customer/customerService.js
+++ src/features/customer/customerService.js
@@ -1,11 +1,21 @@
 import API from "../../services/api";
 
 export const getCustomersApi = async () => {
-  const response = await API.get("/customers");
+  const response = await API.get("/customers/all");
   return response.data;
 };
 
 export const createCustomerApi = async (data) => {
-  const response = await API.post("/customers", data);
+  const response = await API.post("/customers/create", data);
+  return response.data;
+};
+
+export const updateCustomerApi = async (id, data) => {
+  const response = await API.put(`/customers/update/${id}`, data);
+  return response.data;
+};
+
+export const deleteCustomerApi = async (id) => {
+  const response = await API.delete(`/customers/delete/${id}`);
   return response.data;
 };
--- src/pages/customers/CustomerList.jsx
+++ src/pages/customers/CustomerList.jsx
@@ -1,5 +1,177 @@
-const CustomerList = () => {
-    return <h1>Customers</h1>;
+import React, { useEffect, useState } from "react";
+import "./CustomerList.css";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getCustomers,
+  createCustomer,
+  updateCustomer,
+  deleteCustomer,
+} from "../../features/Customer/customerSlice";
+
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import CustomerValidation from "../../validations/CustomerValidation";
+
+const Customer = () => {
+  const dispatch = useDispatch();
+  const { customers } = useSelector((state) => state.customer);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5;
+
+  const indexOfLast = currentPage * itemsPerPage;
+  const indexOfFirst = indexOfLast - itemsPerPage;
+  const currentCustomers = customers.slice(indexOfFirst, indexOfLast);
+  const totalPages = Math.ceil(customers.length / itemsPerPage);
+
+  const [editId, setEditId] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(CustomerValidation),
+  });
+
+  useEffect(() => {
+    dispatch(getCustomers({ page: 1, search: "" }));
+  }, [dispatch]);
+
+  const onSubmit = (data) => {
+    if (editId) {
+      dispatch(updateCustomer({ id: editId, data }));
+      setEditId(null);
+    } else {
+      dispatch(createCustomer(data));
+    }
+    reset();
+    setShowModal(false);
   };
-  
-  export default CustomerList;
+
+  const handleEdit = (customer) => {
+    setEditId(customer._id);
+
+    reset({
+      customerCode: customer.customerCode,
+      customerName: customer.customerName,
+      phone: customer.phone,
+      email: customer.email,
+    });
+    setShowModal(true);
+  };
+
+  return (
+    <div className="customer-container">
+      <button
+        className="customer-header"
+        onClick={() => {
+          setEditId(null);
+          reset();
+          setShowModal(true);
+        }}
+      >
+        Add Customer
+      </button>
+
+      {showModal && (
+        <div className="modal-overlay">
+          <div className="modal">
+            <h2>{editId ? "Update Customer" : "Add Customer"}</h2>
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <input
+                placeholder="Customer Code"
+                {...register("customerCode")}
+              />
+              <p>{errors.customerCode?.message}</p>
+
+              <input
+                placeholder="Customer Name"
+                {...register("customerName")}
+              />
+              <p>{errors.customerName?.message}</p>
+
+              <input placeholder="Phone" {...register("phone")} />
+              <p>{errors.phone?.message}</p>
+
+              <input placeholder="Email" {...register("email")} />
+              <p>{errors.email?.message}</p>
+
+              <button type="submit">{editId ? "Update" : "Save"}</button>
+
+              <button
+                type="button"
+                onClick={() => {
+                  setShowModal(false);
+                  setEditId(null);
+                  reset();
+                }}
+              >
+                Cancel
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
+
+      <table border="1" className="table-container">
+        <thead>
+          <tr>
+            <th>Code</th>
+            <th>Name</th>
+            <th>Phone</th>
+            <th>Email</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {currentCustomers.map((customer, index) => (
+            <tr key={customer._id}>
+              <td>{indexOfFirst + index + 1}</td>
+              <td>{customer.customerCode}</td>
+
+              <td>{customer.customerName}</td>
+
+              <td>{customer.phone}</td>
+
+              <td>{customer.email}</td>
+
+              <td>
+                <button onClick={() => handleEdit(customer)}>Edit</button>
+
+                <button onClick={() => dispatch(deleteCustomer(customer._id))}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="pagination">
+        <button
+          type="button"
+          disabled={currentPage === 1}
+          onClick={() => setCurrentPage((prev) => prev - 1)}
+        >
+          Previous
+        </button>
+
+        <span>
+          Page {currentPage} of {totalPages || 1}
+        </span>
+
+        <button
+          type="button"
+          disabled={currentPage >= totalPages}
+          onClick={() => setCurrentPage((prev) => prev + 1)}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Customer;
