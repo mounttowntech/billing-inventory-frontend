e27142c5cb8b041e81858602f4382e6093541116
vikash-mounttowntech <mounttowntech@gmail.com>
2026-07-07 16:57:24 +0530

    Same type heading CSS color

--- src/pages/season/Season.jsx
+++ src/pages/season/Season.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import "./Season.css";
 import {
   getSeasons,
   createSeason,
@@ -170,7 +171,7 @@ const Season = () => {
         </div>
       )}
 
-      <table border="1" cellPadding="10">
+      <table border="1" cellPadding="10" className="season-table">
         <thead>
           <tr>
             <th>S.No</th>
